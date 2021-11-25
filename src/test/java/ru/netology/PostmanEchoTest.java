ba1ce994844639d8ee5823465e64ec6f30cc4784
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-11-25 16:22:08 +0300

    success

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class PostmanEchoTest {
+    @Test
+            public void shouldPostmanPost(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("some data"));}
+
+
+
+
+}
