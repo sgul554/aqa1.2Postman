6393fb42e1525089491c750a0df917cf63309782
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-11-25 16:28:05 +0300

    failed test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -22,7 +22,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("some data"));}
+                .body("data",equalTo("hello"));}
 
 
 
